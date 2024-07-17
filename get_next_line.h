ea559be3bc07b48a298640bdadd2ad71e67dd6ea
Joseph Kicuma <jquicuma@student.42.fr>
2024-07-17 07:16:00 +0100

    remake: Restarting the last time the project by scratch

--- get_next_line.h
+++ get_next_line.h
@@ -6,23 +6,7 @@
 /*   By: jquicuma <jquicuma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 11:29:05 by jquicuma          #+#    #+#             */
-/*   Updated: 2024/07/11 18:11:17 by jquicuma         ###   ########.fr       */
+/*   Updated: 2024/07/17 07:15:06 by jquicuma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-
-# define GET_NEXT_LINE_H
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
-
-# include <stdlib.h>
-# include <unistd.h>
-/* Bibliotecas importadas para testes */
-# include <fcntl.h>
-# include <stdio.h>
-# include <string.h>
-
-#endif
